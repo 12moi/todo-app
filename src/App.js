702bd983eef9fadf5800e73a36630479d2bc3c2b
12moi <moi.shadrack@student.moringaschool.com>
2022-06-27 16:26:16 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className='App'>
      <header><h1>Moi's Todo List </h1></header>
-     <Form setInputText={setInputText}/>
+     <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
      <TodoList  inputText={inputText}/>
     </div>
   )
