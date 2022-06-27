a280183fad7300149f9fadf96b68d5cf9868e1f4
12moi <moi.shadrack@student.moringaschool.com>
2022-06-27 12:50:35 +0300

    add todolist component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css'
 
 import Form from './Components/Form'
+import TodoList from './Components/TodoList'
 function App() {
   return (
     <div className='App'>
      <header><h1>Moi's Todo List</h1></header>
      <Form />
+     <TodoList />
     </div>
   )
 }
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,1 +1,10 @@
 import React from "react";
+
+const TodoList = ()=>{
+    return(
+      <div className="todo-container">
+        <ul className="todo-list"></ul>
+      </div>
+    )
+}
+export default TodoList
